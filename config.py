d5e5be7809af9bbab89af658d1fb6d60ba30ceb3
Girin325 <girin3943@chungbuk.ac.kr>
2024-12-17 13:48:05 +0900

    commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# API 키
+CLOVA_API_URL = "your_api_url"
+CLOVA_SECRET_KEY = "your_clova_api_key"
+OPENAI_API_KEY = "your_gpt_api_key"
+
+# 파일 경로
+PDF_PATH = "./db/다양한 질병에 따른 식이와 추천 식품.pdf"
+EXCEL_FILE_PATH = "./db/20240807_음식DB.xlsx"
